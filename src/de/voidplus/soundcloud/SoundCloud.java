c22466ad993aed31644d7183c0061d770c53205f
voidplus <mail@voidplus.de>
2014-11-24 00:23:12 +0100

    initialized p5 library

---
+++ src/de/voidplus/soundcloud/SoundCloud.java
@@ -0,0 +1,8 @@
+package de.voidplus.soundcloud;
+
+public class SoundCloud {
+	// https://github.com/voidplus/soundcloud-java-library
+	// https://github.com/voidplus/soundcloud-java-library/blob/master/src/main/java/de/voidplus/soundcloud/SoundCloud.java
+	
+	// TODO Add internal magic stuff (in combination with Minim).
+}
